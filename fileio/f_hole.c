65af1d5e397dd4e226cf5dfba277fe3bad1f131a
Damir Krstinic <dkrst@fesb.hr>
2026-04-22 13:33:15 +0200

    manje ispravke

--- fileio/f_hole.c
+++ fileio/f_hole.c
@@ -7,29 +7,29 @@
 #include <fcntl.h>
 
 int main() {
-    char buf1[]="Prvi redak teksta";
-    char buf2[]="Drugi redak teksta";
-    int fd;
-    
-    fd=creat("file.hole", (mode_t)0644);
-    if (fd==-1) {
-	perror("creat");
-	return -1;
-    }
-
-    if (write(fd, buf1, strlen(buf1)+1)!=strlen(buf1)+1) {
-	perror("write buf1");
-	return -1;
-    }
-
-    if (lseek(fd, 15, SEEK_CUR)==-1) {
-	perror("lseek");
-	return -1;
-    }
-    
-    if (write(fd, buf2, strlen(buf2)+1)!=strlen(buf2)+1) 
-	perror("write buf2");
-    
-    close(fd);
-    exit(0);
+  char buf1[]="Prvi redak teksta";
+  char buf2[]="Drugi redak teksta";
+  int fd;
+  
+  fd=creat("file.hole", (mode_t)0644);
+  if (fd==-1) {
+    perror("creat");
+    return -1;
+  }
+  
+  if (write(fd, buf1, strlen(buf1)+1)!=strlen(buf1)+1) {
+    perror("write buf1");
+    return -1;
+  }
+  
+  if (lseek(fd, 15, SEEK_CUR)==-1) {
+    perror("lseek");
+    return -1;
+  }
+  
+  if (write(fd, buf2, strlen(buf2)+1)!=strlen(buf2)+1) 
+    perror("write buf2");
+  
+  close(fd);
+  exit(0);
 }
--- fileio/f_strip.c
+++ fileio/f_strip.c
@@ -7,29 +7,29 @@
 #include <fcntl.h>
 
 int main() {
-    char buf1[]="Prvi redak teksta";
-    char buf2[]="Drugi redak teksta";
-    int fd;
+  char buf1[]="Prvi redak teksta";
+  char buf2[]="Drugi redak teksta";
+  int fd;
+  
+  fd=open("file.strip", O_WRONLY | O_CREAT | O_TRUNC, (mode_t)0644);
+  if (fd==-1) {
+    perror("open");
+    return -1;
+  }
+  
+  if (write(fd, buf1, strlen(buf1)+1)!=strlen(buf1)+1) {
+    perror("write buf1");
+    return -1;
+  }
     
-    fd=open("file.strip", O_WRONLY | O_CREAT | O_TRUNC, (mode_t)0644);
-    if (fd==-1) {
-	perror("open");
-	return -1;
-    }
-    
-    if (write(fd, buf1, strlen(buf1)+1)!=strlen(buf1)+1) {
-	perror("write buf1");
-	return -1;
-    }
-    
-    if (lseek(fd, 15, SEEK_SET)==-1) {
-	perror("lseek");
-	return -1;
-    }
-    
-    if (write(fd, buf2, strlen(buf2)+1)!=strlen(buf2)+1) 
-	perror("write buf2");
-    
-    close(fd);
-    exit(0);
+  if (lseek(fd, 15, SEEK_SET)==-1) {
+    perror("lseek");
+    return -1;
+  }
+  
+  if (write(fd, buf2, strlen(buf2)+1)!=strlen(buf2)+1) 
+    perror("write buf2");
+  
+  close(fd);
+  exit(0);
 }
